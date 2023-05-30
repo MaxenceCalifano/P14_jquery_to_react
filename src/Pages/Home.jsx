dd5ef52fd34b3bc1c33ff1bf23b2db0b3f0cf930
Maxence <maxence.califano@gmail.com>
2023-05-30 10:53:31 +0300

    set the project up

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>HRnet</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from './Pages/Home';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
