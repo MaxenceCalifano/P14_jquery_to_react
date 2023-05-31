28a729d65762673cf6962ae9ed9c481b342440ea
Maxence <maxence.califano@gmail.com>
2023-05-31 07:57:40 +0300

    copy form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,46 @@ function Home() {
             <main>
                 <Link to={'/employee-list'}>View Current Employees</Link>
                 <h2>Create Employee</h2>
+                <form>
+
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" id="first-name" />
+
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" id="last-name" />
+
+                    <label htmlFor="date-of-birth">Date of Birth</label>
+                    <input id="date-of-birth" type="text" />
+
+                    <label htmlFor="start-date">Start Date</label>
+                    <input id="start-date" type="text" />
+
+                    <fieldset className="address">
+                        <legend>Address</legend>
+
+                        <label htmlFor="street">Street</label>
+                        <input id="street" type="text" />
+
+                        <label htmlFor="city">City</label>
+                        <input id="city" type="text" />
+
+                        <label htmlFor="state">State</label>
+                        <select name="state" id="state"></select>
+
+                        <label htmlFor="zip-code">Zip Code</label>
+                        <input id="zip-code" type="number" />
+                    </fieldset>
+
+                    <label htmlFor="department">Department</label>
+                    <select name="department" id="department">
+                        <option>Sales</option>
+                        <option>Marketing</option>
+                        <option>Engineering</option>
+                        <option>Human Resources</option>
+                        <option>Legal</option>
+                    </select>
+                </form>
+
             </main>
         </div>
     );
