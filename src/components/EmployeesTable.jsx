700e055f5ac2c8e069711d5fe3df12cec0ee63c4
Maxence <maxence.califano@gmail.com>
2023-06-20 12:34:38 +0300

    search works

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -5,38 +5,54 @@ function EmployeeTable({ data, columns }) {
     const [employees, setEmployees] = useState()
 
     useEffect(() => setEmployees(data), [data])
+
+    const searchEmployee = (e) => {
+        const userInput = e.target.value
+        if (userInput < 2) return
+
+        const filteredEmployees = employees.filter(employee => {
+            if (Object.values(employee).some(elem => elem.includes(userInput))) return true
+        })
+        setEmployees(filteredEmployees)
+    }
+
     return (<>
-        {/* 
-            Doit recevoir des datas : en l'occurence employees, venant de localStorage
-            Doit recevoir aussi, chaque champs
-        */}
 
         {
-            employees ? <table>
-                <thead>
-                    <tr>
+            employees ?
+                <>
+                    <div className={styles.search}>
+                        <label>
+                            Search:
+                        </label>
+                        <input onChange={searchEmployee} type='search' />
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
 
-                        {columns.map((col, index) => <th key={index}>{col}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        employees.map((row, index) => (
-                            <tr key={index}>
-                                <td>{row.firstName}</td>
-                                <td>{row.lastName}</td>
-                                <td>{row.startDate}</td>
-                                <td>{row.department}</td>
-                                <td>{row.dateOfBirth}</td>
-                                <td>{row.street}</td>
-                                <td>{row.city}</td>
-                                <td>{row.state}</td>
-                                <td>{row.zipCode}</td>
+                                {columns.map((col, index) => <th key={index}>{col}</th>)}
                             </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                            {
+                                employees.map((row, index) => (
+                                    <tr key={index}>
+                                        <td>{row.firstName}</td>
+                                        <td>{row.lastName}</td>
+                                        <td>{row.startDate}</td>
+                                        <td>{row.department}</td>
+                                        <td>{row.dateOfBirth}</td>
+                                        <td>{row.street}</td>
+                                        <td>{row.city}</td>
+                                        <td>{row.state}</td>
+                                        <td>{row.zipCode}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </>
                 : <></>
         }
 
