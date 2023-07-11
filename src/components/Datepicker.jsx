e5458abaacaa740cf60a3693b5d633b998cee0e0
Maxence <maxence.califano@gmail.com>
2023-07-11 14:51:58 +0400

    get the date values

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -1,50 +1,84 @@
 import dayjs from "dayjs";
-import { useEffect } from "react";
+import { useCallback, useMemo } from "react";
+import PropTypes from 'prop-types'
 import weekday from 'dayjs/plugin/weekday'
 import 'dayjs/locale/fr'
 dayjs.extend(weekday)
 dayjs.locale('fr')
 
 
-function Datepicker() {
+function Datepicker({ selectedDate, setSelectedDate }) {
+    //simule la props
+    const date = dayjs().set('month', 0)// .set('date', 4) peu se chainer comme ça
 
-
-
-    useEffect(() => {
-
-        const date = dayjs().set('month', 0)
-
+    const getCalendarCells = date => {
         const daysToFirstOfTheMonth = date.date(1).weekday() - 1
-        console.log('index of first', date.date(1).weekday())
-        console.log(dayjs.locale())
-        console.log("🚀 ~ file: Datepicker.jsx:9 ~ useEffect ~ daysToFirstOfTheMonth:", daysToFirstOfTheMonth)
+
         const calendarCells = []
         const daysInMonth = date.daysInMonth()
+
+        const prepareCell = (date, dayNumber) => {
+            return {
+                text: String(dayNumber),
+                value: date.clone().set("date", dayNumber)
+            };
+        };
+
         // push current month day cells
         for (let i = 0; i < daysInMonth; i++) {
-            calendarCells.push(i + 1);
+            calendarCells.push(prepareCell(date, i + 1));
         }
         const cellsToAdd = 42 - daysInMonth - (daysToFirstOfTheMonth + 1)
-        console.log("🚀 ~ file: Datepicker.jsx:20 ~ useEffect ~ cellsToAdd:", cellsToAdd)
-
 
         // Gets the number of days between the first day of the month and the monday of the same week, same for the last day
         // add to start from prev month
         const lastMonth = date.subtract(1, "month");
         for (let i = 0; i <= daysToFirstOfTheMonth; i++) {
-            calendarCells.unshift(lastMonth.daysInMonth() - i);
+            calendarCells.unshift(prepareCell(lastMonth, lastMonth.daysInMonth() - i));
         }
 
         // add to end from next month
+        const nextMonth = date.add(1, "month");
         for (let i = 0; i < cellsToAdd; i++) {
-            calendarCells.push(i + 1);
+            calendarCells.push(prepareCell(nextMonth, i + 1));
         }
         //const dateClone = date.clone().set('date', 12)
-        console.log(calendarCells)
-    })
+        return calendarCells
+    }
 
-    return (<>
-    </>);
+    const getCalendarRows = useCallback(date => {
+        const cells = getCalendarCells(date)
+
+        const rows = [];
+
+        // split one array into chunks
+        for (let i = 0; i < cells.length; i += 7) {
+            rows.push(cells.slice(i, i + 7));
+        }
+        console.log("🚀 ~ file: Datepicker.jsx:46 ~ getCalendarRows ~ rows:", rows)
+
+        return rows;
+    }, [])
+
+    const rows = useMemo(() => getCalendarRows(date), [date, getCalendarRows])
+
+    return (
+        <div>
+            {
+                rows.map((cells, rowIndex) => (
+                    <div key={rowIndex}>
+                        {cells.map(({ text, value }, cellIndex) => (
+                            <div key={cellIndex} onClick={() => console.log(value)}>{text}</div>
+                        ))}
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
+Datepicker.propTypes = {
+    selectedDate: PropTypes.object,
+    setSelectedDate: PropTypes.func
+}
 export default Datepicker;
